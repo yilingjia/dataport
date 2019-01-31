454a152aa3de9940bffb4148bc0057a93ef7c44c
ylwander <yj9xs@virginia.edu>
2019-01-31 16:54:51 -0500

    update

--- code/download_metadata.py
+++ code/download_metadata.py
@@ -4,8 +4,8 @@ database_host = 'dataport.pecanstreet.org'
 database_port = '5434'
 database_name = 'postgres'
 database_schema = 'university'
-database_username='dqeRSAITtEVb'
-database_password='GIr6e3WIXpI0'
+database_username='iMnYaDQqKn5f'
+database_password='KFmf95DPQ6T2'
 
 conn = db.connect('host=' + database_host +
                           ' port=' + database_port +
@@ -13,6 +13,6 @@ conn = db.connect('host=' + database_host +
                           ' user=' + database_username +
                           ' password=' + database_password)
 
-sql_query = """SELECT * from university.metadata"""
+sql_query = """SELECT * from university.survey_2017_all_participants"""
 df = pd.read_sql(sql_query, conn)
-df.to_csv("../metadata/metadata.csv", index=False)
+df.to_csv("../metadata/survey_2017_all_participants.csv", index=False)
